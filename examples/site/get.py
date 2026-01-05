1591f071ef844198fc948b254c4b7307f8b85af7
Patrick Lindel <lindel@dda-web.de>
2026-01-05 14:29:56 +0100

    add site example

---
+++ examples/site/get.py
@@ -0,0 +1,12 @@
+import os
+
+import ornitho
+
+ornitho.consumer_key = os.environ.get("ORNITHO_CONSUMER_KEY")
+ornitho.consumer_secret = os.environ.get("ORNITHO_CONSUMER_SECRET")
+ornitho.user_email = os.environ.get("ORNITHO_USER_EMAIL")
+ornitho.user_pw = os.environ.get("ORNITHO_USER_PW")
+ornitho.api_base = os.environ.get("ORNITHO_API_BASE")
+
+site = ornitho.Site.get(id_=26394)
+print(f"Place with ID {site.id_}: {site.custom_name} / {site.local_name}")
