4acc08f7788c0e110734dc09aadfc0006f21c742
Patrick Lindel <lindel@dda-web.de>
2026-03-06 12:58:05 +0100

    add repr, eq and hash to project

--- ornitho/model/project.py
+++ ornitho/model/project.py
@@ -14,3 +14,16 @@ class Project:
 
     def __str__(self) -> str:
         return f"{self.id_}:{self.project_code}"
+
+    def __repr__(self) -> str:
+        return f"<{self.__module__}.{self.__class__.__qualname__} {self}>"
+
+    def __eq__(self, other):
+        return (
+            self.id_ == other.id_
+            and self.project_code == other.project_code
+            and self.project_name == other.project_name
+        )
+
+    def __hash__(self):
+        return hash((self.id_, self.project_code, self.project_name))
